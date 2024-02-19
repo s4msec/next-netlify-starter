57c5b8b47279b9f7526a926e5b6ca1d5a69e442f
s4msec <75855533+s4msec@users.noreply.github.com>
2024-02-19 11:39:03 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,38 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Script from 'next/script'; // Importando o componente next/script
+import { useEffect } from 'react';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
+  useEffect(() => {
+    // Verifica se Service Workers são suportados
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register("/OneSignalSDKWorker.js").then(function(registration) {
+        console.log("Service Worker registrado com sucesso com escopo:", registration.scope);
+      }).catch(function(err) {
+        console.log("Service Worker falhou no registro:", err);
+      });
+    }
+
+    // Inicialização do OneSignal
+    window.OneSignalDeferred = window.OneSignalDeferred || [];
+    OneSignalDeferred.push(function(OneSignal) {
+      OneSignal.init({
+        appId: "0e6d9083-524a-4079-89eb-5a7bfd60b820",
+      });
+    });
+  }, []);
+
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
-        <script>navigator.serviceWorker.register("OneSignalSDKWorker.js")</script>
-        <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-        <script>
-          window.OneSignalDeferred = window.OneSignalDeferred || [];
-          OneSignalDeferred.push(function(OneSignal) {
-            OneSignal.init({
-              appId: "0e6d9083-524a-4079-89eb-5a7bfd60b820",
-            })
-          })
-        </script>
       </Head>
+
+      {/* Usando next/script para carregar o script do OneSignal */}
+      <Script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" strategy="afterInteractive" />
 
       <main>
         <Header title="Welcome to my app!" />
@@ -29,5 +43,5 @@ export default function Home() {
 
       <Footer />
     </div>
-  )
+  );
 }
